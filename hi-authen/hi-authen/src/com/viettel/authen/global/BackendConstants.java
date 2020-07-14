7eccc9aa7fb3f20af764bb0308c19ba1b8c7926c
TruongNX25 <truongnx25@viettel.com.vn>
2020-07-14 15:31:01 +0700

    - Re-structure folder

--- hi-authen/hi-authen/src/com/viettel/authen/global/BackendConstants.java
+++
@@ -1,9 +0,0 @@
-package com.viettel.authen.global;
-
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-
-public class BackendConstants {
-
-    public static final DateFormat DATE_FORMAT = new SimpleDateFormat("dd/MM/yyyy");
-}
