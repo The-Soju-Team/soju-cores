0531cf3ea3b14c06c46a42b774ee6609c73e5500
weirdobeardo48 <truongnx@easyshoppingfeed.com>
2020-09-18 10:10:50 +0700

    - missing files in previous commit

---
+++ hi-cache-server/src/com/hh/cache/constant/MemManagerConstants.java
@@ -0,0 +1,12 @@
+package com.hh.cache.constant;
+
+/**
+ * @author TruongNX25
+ */
+
+public class MemManagerConstants {
+    public static final int CREDENTIALS_ADD_ACTION = 1;
+    public static final int CREDENTIALS_UPDATE_ACTION = 2;
+    public static final int CREDENTIALS_REMOVE_ACTION = 3;
+    public static final int CREDENTIALS_CLONE_ACTION = 4;
+}
